960e1e999ae00b1495e0819d9bc08aaf14cbd14a
Alexander Merz <am@golem.de>
2017-12-04 23:34:54 +0100

    add blink example

--- einheit/dust_sensor_exp/dust_sensor_exp.ino
+++ einheit/dust_sensor_exp/dust_sensor_exp.ino
@@ -2,14 +2,23 @@
 
 FWDust2 fwdust;
 
+unsigned long testtime = 0;
+bool state = 0;
+
 void setup() {
   Serial.begin(300);
   fwdust.init();
   fwdust.setCallback(onSensor);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   fwdust.check();
+  if(testtime < millis()) {
+    digitalWrite(13, state);
+    testtime = millis()+500;
+    state = !state;
+  }
 }
 
 void onSensor(Framework &sensor) {
